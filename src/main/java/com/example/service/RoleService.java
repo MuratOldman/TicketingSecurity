0c697d00d04d8c3ca2fc540808f59f2a50dc56b3
MuratOldman <muratkara15102@gmail.com>
2023-01-17 13:05:37 -0500

    Initial commit

---
+++ src/main/java/com/example/service/RoleService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+
+
+import com.example.dto.RoleDTO;
+
+import java.util.List;
+
+public interface RoleService {
+
+    List<RoleDTO> listAllRoles();
+    RoleDTO findById(Long id);
+}
---
+++ src/main/java/com/example/ticketingsecurity/TicketingSecurityApp.java
@@ -0,0 +1,13 @@
+package com.example.ticketingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSecurityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingSecurityApp.class, args);
+    }
+
+}
